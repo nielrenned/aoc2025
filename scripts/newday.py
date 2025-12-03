003e7f3632f2e04f11310e36f01354b144074422
Jacob Dennerlein <nielrenned@gmail.com>
2025-12-02 20:58:49 -0500

    Update template script

--- scripts/newday.py
+++ scripts/newday.py
@@ -1,11 +1,27 @@
-import requests
 import sys
 import os
 
-template = """DAY = {day}
-RAW_INPUT = None
-INPUT = None
+template = """def main():
+    load_input(use_test_input=False)
+    parse_input()
+    print('PART 1:', part1())
+    # print('PART 2:', part2())
 
+    
+def parse_input():
+    global INPUT
+    INPUT = []
+    for line in RAW_INPUT.split('\\n'):
+        if line == '': continue
+        INPUT.append(line)
+
+        
+def part1():
+    pass
+
+
+def part2():
+    pass
 
 def load_input(use_test_input=False):
     global RAW_INPUT
@@ -16,29 +32,10 @@ def load_input(use_test_input=False):
         RAW_INPUT = f.read()
 
 
-def parse_input():
-    global INPUT
-    INPUT = []
-    for line in RAW_INPUT.split('\\n'):
-        if line == '': continue
-        INPUT.append(line)
-
-
-def part1():
-    pass
-
-
-def part2():
-    pass
-
-
-def main():
-    load_input(use_test_input=False)
-    parse_input()
-    print('PART 1:', part1())
-    # print('PART 2:', part2())
-
-
+DAY = {day}
+RAW_INPUT = None
+INPUT = None
+        
 if __name__ == "__main__":
     main()
 """
@@ -59,24 +56,13 @@ def main():
         print(f'Error: {solution_filename} already exists.')
         return
     
-    input_filename = f'inputs/actual/day{day_num:02}.txt'
-    if os.path.exists(input_filename):
-        print(f'Error: {input_filename} already exists.')
-        return
-    
-    test_input_filename = f'inputs/test/day{day_num:02}.txt'
-    if os.path.exists(test_input_filename):
-        print(f'Error: {test_input_filename} already exists.')
-        return
-    
     with open(solution_filename, 'w') as f:
         f.write(template.format(day=day_num))
-    
-    with open(input_filename, 'w'):
-        pass
 
-    with open(test_input_filename, 'w'):
-        pass
+    test_input_filename = f'inputs/test/day{day_num:02}.txt'
+    if not os.path.exists(test_input_filename):
+        with open(test_input_filename, 'w') as f:
+            pass
 
     print('Files created!')
 
