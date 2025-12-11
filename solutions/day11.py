1f6cfc1985b0ac485cb9c35324a0cbca73acc5e6
Jacob Dennnerlein <nielrenned@gmail.com>
2025-12-11 09:45:51 -0500

    Day 11 Faster!

--- solutions/day11.py
+++ solutions/day11.py
@@ -1,6 +1,3 @@
-from queue import Queue
-from functools import cache
-
 def parse_input(raw_input: str, is_test_input: bool):
     input = {}
     for line in raw_input.split('\n'):
@@ -11,50 +8,29 @@ def parse_input(raw_input: str, is_test_input: bool):
     return input
 
 
-def subgraph_before(v, graph):
-    subgraph = {}
-    q = Queue()
-    q.put(v)
-    while not q.empty():
-        node = q.get()
-        for source, sinks in graph.items():
-            if node not in sinks: continue
-            if source not in subgraph:
-                subgraph[source] = sinks
-                q.put(source)
-    return subgraph
+def count_paths(start, goal, graph, cache):
+    if start in cache:       return cache[start]
+    if start not in graph:   return 0
+    if goal in graph[start]: return 1
 
-
-def count_paths(start, goal, graph):
-    subgraph = subgraph_before(goal, graph)
-    q = Queue()
-    q.put(start)
-    count = 0
-    while not q.empty():
-        node = q.get()
-        if node not in subgraph: continue
-        for sink in subgraph[node]:
-            if sink == goal:
-                count += 1
-            else:
-                q.put(sink)
-    return count
+    result = sum(count_paths(node, goal, graph, cache) for node in graph[start])
+    cache[start] = result
+    return result
 
 
 def part1(input):
-    return count_paths('you', 'out', input)
+    return count_paths('you', 'out', input, {})
 
 
 def part2(input):
-    if 'fft' in subgraph_before('dac', input):
-        return (
-            count_paths('svr', 'fft', input) *
-            count_paths('fft', 'dac', input) *
-            count_paths('dac', 'out', input)
-        )
-    else:
-        return (
-            count_paths('svr', 'dac', input) *
-            count_paths('dac', 'fft', input) *
-            count_paths('fft', 'out', input)
-        )
+    # We could do some cache sharing, but this is fast enough
+    # on my machine, that I don't care to clutter up the code
+    return (
+        count_paths('svr', 'fft', input, {}) *
+        count_paths('fft', 'dac', input, {}) *
+        count_paths('dac', 'out', input, {})
+        +
+        count_paths('svr', 'dac', input, {}) *
+        count_paths('dac', 'fft', input, {}) *
+        count_paths('fft', 'out', input, {})
+    )
